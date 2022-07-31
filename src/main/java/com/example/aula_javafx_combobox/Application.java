789d740bf30ef1c3a88587124f2a1140401d9436
guinn83 <guinn83@hotmail.com>
2022-07-31 20:08:01 -0300

    Project created

---
+++ src/main/java/com/example/aula_javafx_combobox/Application.java
@@ -0,0 +1,26 @@
+package com.example.aula_javafx_combobox;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Sample JavaFX application");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
